75d65894138cbcd259154e7d4727a989333f3c27
Brit Butler <redline6561@gmail.com>
2017-03-22 11:01:58 -0400

    We got it working ... but quadratic time? :scream:

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import DICTIONARY from './dictionary';
 
+var startButton = document.querySelector(".find-words");
+var wordlist = document.querySelector(".words");
+var testedCount = 0;
+
 function isAnagram (word1, word2) {
   let sorted1 = word1.split("").sort().join("");
   let sorted2 = word2.split("").sort().join("");
@@ -14,5 +18,25 @@ function hasJoiner (word1, word2) {
 function isFunnyWord (word1, word2) {
   return hasJoiner(word1, word2) && isAnagram(word1, word2);
 };
+
+function funnyWordTmpl (word1, word2) {
+  return `<li>${word1}${word2.slice(1)}</li>`;
+};
+
+function findFunnyWords (event) {
+  console.time();
+  DICTIONARY.forEach(word1 => {
+    DICTIONARY.forEach(word2 => {
+      testedCount++;
+      if (isFunnyWord(word1, word2)) {
+        wordlist.innerHTML += funnyWordTmpl(word1, word2);
+      }
+    });
+  });
+  console.timeEnd();
+  console.log(`Checked ${testedCount} word combinations.`);
+};
+
+startButton.addEventListener('click', findFunnyWords);
 
 export { isAnagram, hasJoiner, isFunnyWord };
